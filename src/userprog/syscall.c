403070efc475ada1ae7a13e3bc97201eb67c3f44
Oliver Ni <oliver.ni@gmail.com>
2023-09-20 18:46:03 -0700

    [proj1] implement write syscall for stdout

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -26,4 +26,14 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
     printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
     process_exit();
   }
+
+  if (args[0] == SYS_WRITE) {
+    int fd = args[1];
+    const void* buffer = args[2];
+    unsigned size = args[3];
+
+    if (fd == 1) {
+      putbuf(buffer, size);
+    }
+  }
 }
